e7487eb51abb512cfbb60e1641b9b064e66ff338
Rene Ismael <Ismael_br7@hotmail.com>
2019-12-18 10:44:53 -0600

    Ejercicio 2 ok

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ const resultFinal = arrayNums.reduce((total, currentValue) => {
 });
 
 console.log(resultFinal);
+
+// Pregunta 2
+// Closure y como funciona
+
+// Definicion funciones que van a estar declarada dentro de otra funcion y tiene acceso a los valores de
+// su funcion padre, pero al ser al utilizar var se puede redeclarar la variable nombe
+// en este caso lo ideal es usar let por ser una variable block scope
+
+function padre(nombre) {
+  var nombre = "Ismael";
+
+  function hija() {
+    var nombre = "Irving";
+    console.log(`hola soy ${nombre}`);
+  }
+
+  hija();
+}
+
+padre("Ismael principal");
