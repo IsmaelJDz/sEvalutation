60920166998feeb6864f42ddd268afd3a2462758
Rene Ismael <Ismael_br7@hotmail.com>
2019-12-18 10:40:16 -0600

    Ejercicio 1 ok

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("Prueba");
+//Pregunta 1
+
+let arrayNums = [1, 2, 3, 4];
+
+const resultFinal = arrayNums.reduce((total, currentValue) => {
+  return total + currentValue;
+});
+
+console.log(resultFinal);
